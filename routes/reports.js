aa5e9b8271e12452e4fa37002a70f2415f289392
Richard Polidoro <armyofbear136@gmail.com>
2022-11-17 14:39:49 -0600

    reports generating with correct info for v1 and downloading

--- routes/reports.js
+++ routes/reports.js
@@ -234,8 +234,9 @@ reportsRouter.post('/', async function (req, res, next) {
   //generate csv for selected doors in timeframe
 
   doorsData = await mySQLFun.getDoorsInfo(db, req.params.siteID);
+  usersData = await mySQLFun.getUsersInfo(db, req.params.siteID);
 
-  if (doorsReport){
+  if (doorsReport && req.body.logtype == "event"){
 
 
     for (i in doorsData){
@@ -285,12 +286,85 @@ reportsRouter.post('/', async function (req, res, next) {
     }
     
     // console.log(selectedDoorEvents);
-    await CSVfun.generate((reportTitle + '/' + req.body.name + '_' + 'Doors_' + Date.now() + '.csv'), selectedDoorEvents);
+    await CSVfun.generate((reportTitle + '/' + req.body.name + '_' + 'DoorEvents_' + Date.now() + '.csv'), selectedDoorEvents);
   }
+
+
+  //generate csv for door access report 
+
+  else if (doorsReport && req.body.logtype == 'access'){
+    console.log("access report");
+
+    for (i in doorsData){
+      doorObjects[doorsData[i].name] = {};
+      for (o in doorsData[i]){
+        doorObjects[doorsData[i].name][o] = doorsData[i][o];
+      }
+    }
+    for (i in usersData){
+      userObjects[usersData[i].fob_id] = {};
+      for (o in usersData[i]){
+        userObjects[usersData[i].fob_id][o] = usersData[i][o];
+      }
+    }
+
+      
+  if (typeof req.body.siteusergroups == 'string'){req.body.siteusergroups = [req.body.siteusergroups]};
+  if (typeof req.body.sitedoorgroups == 'string'){req.body.sitedoorgroups = [req.body.sitedoorgroups]};
+  let selectedDoorEvents = [
+    [
+      "cardraw",
+      "cardnr",
+      "access_time",
+      "reader",
+      "id",
+      "first_name",
+      "last_name",
+      "email",
+      "fob_id",
+      "company_id_users",
+      "site_id_users",
+      "employee_id",
+      "fob_raw",
+      "last_access",
+      "companyName",
+      "orgName",
+      "siteName",
+      "doorgroups",
+      "doorname"
+    ]
+  ];
+  // console.log(parsedAllEvents)
+
+    for (var e = 1; e < parsedAllEvents.card_events.length; e++)
+    {
+      var doorMatch = false;
+      let cardEvent = parsedAllEvents.card_events[e];
+      console.log(cardEvent);
+      if ((new Date(cardEvent[2]) > (startDT)) && (new Date(cardEvent[2]) < (endDT)))
+      {
+        for (d in doorObjects){
+          if (doorObjects[d].reader == cardEvent[3]){
+            if (req.body.dooronlygroups.includes(d)){doorMatch = true;}
+            for (o in userObjects[cardEvent[1]]){
+              cardEvent.push(userObjects[cardEvent[1]][o]);
+            }
+            cardEvent.push(d);
+          }
+        }
+        if (doorMatch){
+          selectedDoorEvents.push(cardEvent);
+        }
+      }
+    }
+  // console.log(selectedUserEvents);
+  console.log('generating door access report');
+  await CSVfun.generate((reportTitle + '/' + req.body.name + '_' + 'DoorAccess_' + Date.now() + '.csv'), selectedDoorEvents);
+}
 
   //generate csv for selected users in timeframe
 
-  usersData = await mySQLFun.getUsersInfo(db, req.params.siteID);
+  
 
   if (usersReport){
 
@@ -299,6 +373,13 @@ reportsRouter.post('/', async function (req, res, next) {
             userObjects[usersData[i].fob_id] = {};
             for (o in usersData[i]){
               userObjects[usersData[i].fob_id][o] = usersData[i][o];
+            }
+          }
+
+          for (i in doorsData){
+            doorObjects[doorsData[i].name] = {};
+            for (o in doorsData[i]){
+              doorObjects[doorsData[i].name][o] = doorsData[i][o];
             }
           }
         
@@ -322,7 +403,8 @@ reportsRouter.post('/', async function (req, res, next) {
         "companyName",
         "orgName",
         "siteName",
-        "doorgroups"
+        "doorgroups",
+        "doorname"
       ]
     ];
     // console.log(parsedAllEvents)
@@ -333,10 +415,23 @@ reportsRouter.post('/', async function (req, res, next) {
         let cardEvent = parsedAllEvents.card_events[e];
         if ((cardEvent[1] == req.body.useronlygroups[i]) && (new Date(cardEvent[2]) > (startDT)) && (new Date(cardEvent[2]) < (endDT)))
         {
-          for (o in userObjects[cardEvent[1]]){
-            cardEvent.push(userObjects[cardEvent[1]][o]);
+
+
+          for (d in doorObjects){
+            if (doorObjects[d].reader == cardEvent[3]){
+              for (o in userObjects[cardEvent[1]]){
+                cardEvent.push(userObjects[cardEvent[1]][o]);
+              }
+              cardEvent.push(d);
+            }
           }
           selectedUserEvents.push(cardEvent);
+
+
+          // for (o in userObjects[cardEvent[1]]){
+          //   cardEvent.push(userObjects[cardEvent[1]][o]);
+          // }
+          // selectedUserEvents.push(cardEvent);
         }
       }
     }
@@ -345,6 +440,9 @@ reportsRouter.post('/', async function (req, res, next) {
     await CSVfun.generate((reportTitle + '/' + req.body.name + '_' + 'Users_' + Date.now() + '.csv'), selectedUserEvents);
   }
   // res.download('./csv_export/'+userFileName);
+
+
+  
 
   //generate csv for door and user cross reference 
 
@@ -417,6 +515,7 @@ reportsRouter.post('/', async function (req, res, next) {
   console.log('generating site report');
   await CSVfun.generate((reportTitle + '/' + req.body.name + '_' + 'Site_' + Date.now() + '.csv'), selectedSiteEvents);
 }
+
 
 
   //updates db with most recent statuses
@@ -492,6 +591,8 @@ reportsRouter.post('/', async function (req, res, next) {
 
 });
 
+
+
 reportsRouter.get('/download/:filename', async (req, res)=>{
   console.log('downloading');
 
@@ -506,6 +607,167 @@ reportsRouter.get('/download/:filename', async (req, res)=>{
   });
 
   // res.redirect(`/org/${req.params.orgID}/company/${req.params.companyID}/site/${req.params.siteID}/reports`)
+
+});
+
+/* GET report page. */
+reportsRouter.get('/view/:reportID', async function (req, res, next) {
+
+  console.log(req.params.reportID);
+
+
+  /* link to database */
+
+  var db = req.app.get('db');
+
+  // /* load data from database */
+
+
+  try {
+
+    console.log('Pulling data from door_groups table on reports route');
+    await db.query(
+
+
+
+      `SELECT
+          c.id as id,
+          c.name as companyName,
+          c.org as orgName,
+          s.id as siteID,
+          s.name as siteName,
+          GROUP_CONCAT ( DISTINCT s.doorNames ORDER BY s.doorNames SEPARATOR ', ' ) as doorNamesList,
+          GROUP_CONCAT ( DISTINCT s.doorIDs ORDER BY s.doorNames SEPARATOR ', ' ) as doorIDsList,
+          GROUP_CONCAT ( DISTINCT s.first_name, " ",s.last_name ORDER BY s.last_name SEPARATOR ', ' ) as userFullNamesList,
+          GROUP_CONCAT ( DISTINCT s.userID ORDER BY s.last_name SEPARATOR ', ' ) as userIDsList,
+          GROUP_CONCAT ( DISTINCT s.fobID ORDER BY s.last_name SEPARATOR ', ' ) as userFOBsList
+
+        FROM companies c
+        JOIN (
+          SELECT
+            sit.id as id,
+            sit.name as name,
+            sit.company_id_sites as company_id_sites,
+            dr.doorNames as doorNames,
+            dr.doorIDs as doorIDs,
+            dr.first_name as first_name,
+            dr.last_name as last_name,
+            dr.userID as userID,
+            dr.fobID as fobID
+
+          FROM sites sit
+          JOIN (
+            SELECT
+              drs.site_id_doors as site_id_doors,
+              drs.name as doorNames,
+              drs.id as doorIDs,
+              urs.first_name as first_name,
+              urs.last_name as last_name,
+              urs.userIDs as userID,
+              urs.fobIDs as fobID
+
+            FROM doors drs
+            JOIN (
+              SELECT
+                u.first_name as first_name,
+                u.last_name as last_name,
+                u.id as userIDs,
+                u.fob_id as fobIDs,
+                u.site_id_users as site_id_users
+
+              FROM users u
+
+            ) urs ON (drs.site_id_doors = urs.site_id_users)
+
+          ) dr ON (sit.id = dr.site_id_doors)
+
+        ) s ON (c.id = s.company_id_sites)
+ 
+
+        WHERE s.id = ${req.params.siteID}
+        GROUP BY s.id
+        `,
+
+      async function (err, result, fields) {
+
+        if (err) {
+          console.log(typeof (err));
+          for (var k in err) {
+            console.log(`${k}: ${err[k]}`);
+          }
+          res.render('error', {
+            error: "Server Error", message: "Please try again", sidebar: [
+              { status: 0, url: `/`, icon: "logout", text: "Portal" }], sideTitle: "CCSI Door Access", navTitle: "Server Error 500"
+          });
+          // throw err
+        };
+
+        let groupData = result[0];
+        groupData.allDoors = [];
+        groupData.allUsers = [];
+
+        if (groupData.doorNamesList) {
+          let doorNamesArray = groupData.doorNamesList.split(", ");
+          let doorIDsArray = groupData.doorIDsList.split(", ");
+
+          groupData.allDoors = [];
+          for (i in doorNamesArray) {
+            groupData.allDoors.push({ name: doorNamesArray[i], id: doorIDsArray[i] });
+          }
+        }
+
+        if (groupData.userFullNamesList) {
+          let userNamesArray = groupData.userFullNamesList.split(", ");
+          let userIDsArray = groupData.userIDsList.split(", ");
+          let userFOBsArray = groupData.userFOBsList.split(", ");
+          for (i in userNamesArray) {
+            groupData.allUsers.push({ name: userNamesArray[i], id: userIDsArray[i], fob_id: userFOBsArray[i]});
+          }
+          console.log(groupData.allUsers);
+        }
+
+        let companyName = groupData.companyName;
+        let siteName = groupData.siteName;
+
+        const funSites = await mySQLFun.getSites(db, req.params.companyID)
+
+        var sidebarList = [
+          { status: 0, url: `/org/${req.params.orgID}`, icon: "home", text: "Home" },
+          { status: 0, url: `/org/${req.params.orgID}/company/${req.params.companyID}`, icon: "business", text: `${companyName}` }
+        ];
+        for (i in funSites) {
+          if (funSites[i].id == req.params.siteID) {
+            sidebarList.push({ status: 1, url: `/org/${req.params.orgID}/company/${req.params.companyID}/site/${funSites[i].id}`, icon: "store", text: `${funSites[i].name}` });
+          }
+          else {
+            sidebarList.push({ status: 0, url: `/org/${req.params.orgID}/company/${req.params.companyID}/site/${funSites[i].id}`, icon: "store", text: `${funSites[i].name}` });
+          }
+        }
+
+        var tabBarList = [
+          { status: 0, url: `/org/${req.params.orgID}/company/${req.params.companyID}/site/${req.params.siteID}/doors`, icon: "meeting_room", text: "Doors" },
+          { status: 0, url: `/org/${req.params.orgID}/company/${req.params.companyID}/site/${req.params.siteID}/users`, icon: "person", text: "Users" },
+          { status: 0, url: `/org/${req.params.orgID}/company/${req.params.companyID}/site/${req.params.siteID}/groups`, icon: "supervisor_account", text: "Groups" },
+          { status: 1, url: `/org/${req.params.orgID}/company/${req.params.companyID}/site/${req.params.siteID}/reports`, icon: "summarize", text: "Reports" }
+        ];
+
+        var varName;
+        varName = "CCSI Door Access" //optional title override
+
+        
+
+
+        res.render('reports', { group: groupData, sidebar: sidebarList, tabBar: tabBarList, sideTitle: varName, navTitle: `${companyName} - ${siteName}`, panelTitle: `Door Access Reporting`, panelSubtext: "Select parameters for report", orgID: req.params.orgID, companyID: req.params.companyID, siteID: req.params.siteID });
+
+
+      });
+
+  } catch (err) {
+    console.log(err)
+  } finally {
+    //await db.close();
+  }
+
 
 });
 
